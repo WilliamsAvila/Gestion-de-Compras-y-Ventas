bb564f0ee5257f8bb82af8761c6bc2808657c53c
Williams Avila <molinamolinaa54@gmail.com>
2024-11-03 19:02:32 +0100

    CustomerService with Metho Add, Update and Delete

--- FinalProject/src/main/java/org/amazon/finalproject/Service/CustomerService.java
+++ FinalProject/src/main/java/org/amazon/finalproject/Service/CustomerService.java
@@ -24,11 +24,39 @@ public class CustomerService {
         Optional<Customer> findCustomerById = customerRepository.findById(id);
         if (findCustomerById.isPresent()) {
             Customer foundCustomer = findCustomerById.get();
-            if(!Objects.equals(customer.getName(), foundCustomer.getName()) && customer.getName()!=null) {
+            if (!Objects.equals(customer.getName(), foundCustomer.getName()) && customer.getName() != null) {
                 foundCustomer.setName(customer.getName());
-
             }
-        }
+            if (!Objects.equals(customer.getPassword(), foundCustomer.getPassword()) && customer.getPassword() != null) {
+                foundCustomer.setPassword(customer.getPassword());
+            }
+            if (!Objects.equals(customer.getEmail(), foundCustomer.getEmail()) && customer.getEmail() != null) {
+                foundCustomer.setEmail(customer.getEmail());
+            }
+            if (!Objects.equals(customer.getPhone(), foundCustomer.getPhone()) && customer.getPhone() != null) {
+                foundCustomer.setPhone(customer.getPhone());
+            }
+            if (!Objects.equals(customer.getAddress(), foundCustomer.getAddress()) && customer.getAddress() != null) {
+                foundCustomer.setAddress(customer.getAddress());
+            }
+            if (!Objects.equals(customer.getTypeOfUser(), foundCustomer.getTypeOfUser())) {
+                foundCustomer.setTypeOfUser(customer.getTypeOfUser());
+            }
+            Customer updatedCustomer = customerRepository.save(foundCustomer);
+            return Optional.of(updatedCustomer);
 
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Customer> deleteCustomer(Integer id) {
+        Optional<Customer> findCustomerById = customerRepository.findById(id);
+        if (findCustomerById.isPresent()) {
+            Customer foundCustomer = findCustomerById.get();
+            customerRepository.delete(foundCustomer);
+            return Optional.of(foundCustomer);
+        } else
+            return Optional.empty();
     }
 }
